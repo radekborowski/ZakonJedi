0a746d58b2796425a37ca186c2f5118dd1c9088d
radekborowski <radek.borowski@o2.pl>
2020-02-09 16:12:15 +0100

    R2D2 v2

--- src/main/java/R2D2.java
+++ src/main/java/R2D2.java
@@ -1,4 +1,24 @@
 public class R2D2 {
     int szybkoscporuszania;
     int wagawlasna;
+
+    int mocRazeniaPradem;
+    double szybkoscNaprawcza;
+
+    public R2D2(int szybkoscporuszania, int wagawlasna, int mocRazeniaPradem, double szybkoscNaprawcza) {
+        this.szybkoscporuszania = szybkoscporuszania;
+        this.wagawlasna = wagawlasna;
+        this.mocRazeniaPradem = mocRazeniaPradem;
+        this.szybkoscNaprawcza = szybkoscNaprawcza;
+    }
+
+    @Override
+    public String toString() {
+        return "R2D2{" +
+                "szybkoscporuszania=" + szybkoscporuszania +
+                ", wagawlasna=" + wagawlasna +
+                ", mocRazeniaPradem=" + mocRazeniaPradem +
+                ", szybkoscNaprawcza=" + szybkoscNaprawcza +
+                '}';
+    }
 }
