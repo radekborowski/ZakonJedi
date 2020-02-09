3df2bb107628c8f23fccbd33f171d1646ca992f5
radekborowski <radek.borowski@o2.pl>
2020-02-09 16:21:59 +0100

    Add Imperium

---
+++ src/main/java/GwizadaSmierci.java
@@ -0,0 +1,25 @@
+public class GwizadaSmierci {
+    int zasiegRazenia;
+    int moc;
+
+    public GwizadaSmierci(int zasiegRazenia, int moc) {
+        this.zasiegRazenia = zasiegRazenia;
+        this.moc = moc;
+    }
+
+    public int getZasiegRazenia() {
+        return zasiegRazenia;
+    }
+
+    public void setZasiegRazenia(int zasiegRazenia) {
+        this.zasiegRazenia = zasiegRazenia;
+    }
+
+    public int getMoc() {
+        return moc;
+    }
+
+    public void setMoc(int moc) {
+        this.moc = moc;
+    }
+}
